fd8a8f785b0c703ce2dc4f3f98520c5b4f7ea5a6
killerwolf5899 <patricksisk04@gmail.com>
2024-12-09 01:34:01 -0700

    Revert "mdshbv"
    This reverts commit df2845f4ae43d423b416cb3eeca88b0cde351412.

--- Submissions/Lab7/top2.v
+++ Submissions/Lab7/top2.v
@@ -22,7 +22,7 @@
 
 
 module top2(
-Clk, Reset, V0, V1
+Clk, Reset, minX, minY
     );
     
     //Global Variables
@@ -90,8 +90,9 @@ Clk, Reset, V0, V1
     
     wire BranchE, BranchM;
     wire BranchW;
-    output wire [31:0] V0, V1;
+    wire [31:0] V0, V1;
     
+    output wire [31:0] minX, minY;
     
     wire MemtoRegSAD1;
     wire RegWriteSAD1;
@@ -118,35 +119,44 @@ Clk, Reset, V0, V1
     wire BranchSAD2;
     
     wire [1:0] MFSel;
-    wire [31:0] addrS1, addrS2, addrW;
     
-    wire [1:0] SADD, SADE, SADM;
+    wire [1:0] SAMD, SADE, SADM, SADSAD1, SADSAD2;
+    
+    wire [31:0] FADM, FADSAD1, FADSAD2, FADW;
+    
+    wire [31:0] V1M, V2M, V3M, V4M, V5M, V6M, V7M, V8M, V9M ,V10M, V11M, V12M, V13M, V14M, V15M, V16M;
+    
+    wire [31:0] V1SAD1_Frame, V2SAD1_Frame, V3SAD1_Frame, V4SAD1_Frame, V5SAD1_Frame, V6SAD1_Frame, 
+    V7SAD1_Frame, V8SAD1_Frame, V9SAD1_Frame, V10SAD1_Frame, V11SAD1_Frame, V12SAD1_Frame, V13SAD1_Frame, 
+    V14SAD1_Frame, V15SAD1_Frame, V16SAD1_Frame;
+     
+     wire [31:0] V1SAD1_Window, V2SAD1_Window, V3SAD1_Window, V4SAD1_Window, V5SAD1_Window, 
+     V6SAD1_Window, V7SAD1_Window, V8SAD1_Window, V9SAD1_Window, V10SAD1_Window, V11SAD1_Window, 
+     V12SAD1_Window, V13SAD1_Window, V14SAD1_Window, V15SAD1_Window, V16SAD1_Window;
+    
+    wire [31:0] V1SAD1, V2SAD1, V3SAD1, V4SAD1, V5SAD1, V6SAD1, V7SAD1, V8SAD1, V9SAD1, V10SAD1, 
+    V11SAD1, V12SAD1, V13SAD1, V14SAD1, V15SAD1, V16SAD1;
+    
+    wire [31:0] V1SAD2, V2SAD2, V3SAD2, V4SAD2, V5SAD2, V6SAD2, V7SAD2, V8SAD2, V9SAD2, V10SAD2,
+     V11SAD2, V12SAD2, V13SAD2, V14SAD2, V15SAD2, V16SAD2;
+     
     
     
     
     
-
     
-    
-    
-    //(* MARK_DEBUG = "TRUE" *) output reg [31:0] PCPLUS4;
-    
-    //(* MARK_DEBUG = "TRUE" *) output wire [31:0] InstructioW;
-    
-    //Fetch Stage
-    //ProgramCounter PCCounter(PCInF, PCOutF, Reset, Clk, ~Stall);
-    //PCAdder PCAdder(PCOutF, PCPlus4F);
-    //InstructionMemory InstructionMemory(PCOutF, InstructionF);
     
    Fetch_sub_mod Fetch_sub_mod(Clk, Reset, PCPlus4F, InstructionF, PCInF);
     
     Pipline_Fetch Pipline_Fetch(Clk, PCPlus4F, InstructionF, PCPlus4D, InstructionD, Reset);
-    
+     
+     
+  
     
     Decode_sub_mod Decode_sub_mod(InstructionD, MemReadD, MemToRegD, ALUOpD, MemWriteD, ALUSrcD, 
     RegWriteD, BranchTypeD, jalD, DisplayD, hazardTypeD, WriteRegD1, Clk, WriteRegW,
     WriteDataW, RegWriteW, ReadData1D, ReadData2D, V0, V1, ImmExtD, BranchD,
-    PCSel, PCPlus4D, PCPlus4F, PCInF, WriteRegD, SADD);
+    PCSel, PCPlus4D, PCPlus4F, PCInF, WriteRegD, SAMD);
     
     
     Pipline_Decode Pipline_Decode(Clk,
@@ -155,14 +165,8 @@ Clk, Reset, V0, V1
         MemReadE, MemToRegE, MemWriteE, ALUSrcE, RegWriteE, MemTypeE,
         ALUOpE, WriteRegE, ImmExtE, ReadData1E, ReadData2E, ShftAmtE,
         PCPlus4D, PCPlus4E, jalD, jalE, DisplayD, DisplayE, BranchTypeD, BranchTypeE,
-        hazardTypeD, hazardTypeE, InstructionD, InstructionE, Reset, BranchD, BranchE, SADD, SADE);
-        
-    //Execute Stage
-    //Mux32Bit2To1 ALUSrcMux(ALUSrcValE, ReadData2E, ImmExtE, ALUSrcE);
-     
-    //ALU_Controller ALU_Controller(ALUOpE, ImmExtE[5:0], ALUControlE);
-     
-    //ALU32Bit ALU(ALUControlE, ReadData1E, ALUSrcValE, ShftAmtE, ALUResultE, Zero, PCPlus4E);
+        hazardTypeD, hazardTypeE, InstructionD, InstructionE, Reset, BranchD, BranchE, SAMD, SAME);
+
     
     Execute_sub_mod Execute_sub_mod(ReadData2E, ImmExtE, ALUSrcE, ALUOpE, ReadData1E, ShftAmtE,
      PCPlus4E, ALUResultE, Zero);
@@ -174,44 +178,62 @@ Clk, Reset, V0, V1
         ALUResultM, ReadData2M, WriteRegM,
         MemTypeE, MemTypeM, PCPlus4E, PCPlus4M,
         jalE, jalM, DisplayE, DisplayM, BranchTypeE, BranchTypeM,
-        hazardTypeE, hazardTypeM, InstructionE, InstructionM, Reset, BranchE, BranchM, SADE, SADM);
+        hazardTypeE, hazardTypeM, InstructionE, InstructionM, Reset, BranchE, BranchM, SAME, SADM);
     
-    //Memory Stage
-    //DataMemory DataMemory(ALUResultM, ReadData2M, Clk, MemWriteM, MemReadM,
-     //MemReadDataM, MemTypeM, Reset); 
+ 
      
     forwardingUnit forwardingUnit(instructionW, instructionM, instructionSAD2, MFSel);
     
-    Mux32Bit4to1(AddressM, ALUResultM, addrS1, addrS2, addrW, MFSel);
+    Mux32Bit4to1 Finchat(AddressM, ALUResultM, FADS1, FADS2, FADW, MFSel);
      
-    Memory_sub_mod(AddressM, WriteDataM, ReadData2M, Clk, MemWriteM, MemReadM, 
-    MemTypeM, Reset, vector, address, rowSkip, updatedAddress);    
+    Memory_sub_mod Memory_sub_mod(AddressM, WriteDataM, ReadData2M, Clk, MemWriteM, MemReadM, 
+    MemTypeM, Reset, SADM, FADM, V1M, V2M, V3M, V4M, 
+    V5M, V6M, V7M, V8M, V9M ,V10M, V11M, V12M, V13M, V14M, V15M, V16M);    
     
     Pipline_Memory Pipline_Memory(Clk, MemtoRegM, RegWriteM, MemReadDataM, ALUResultM, WriteRegM,
     MemtoRegSAD1, RegWriteSAD1, MemReadDataSAD1, ALUResultSAD1, WriteRegSAD1,
     PCPlus4M, PCPlus4SAD1, jalM, jalSAD1, DisplayM, DisplaySAD1, BranchTypeM, BranchTypeSAD1, Reset,
-    hazardTypeM, hazardTypeSAD1, instructionM, instructionSAD1, BranchM, BranchSAD1);
+    hazardTypeM, hazardTypeSAD1, instructionM, instructionSAD1, BranchM, BranchSAD1, SADM, SADSAD1, 
+    V1M, V2M, V3M, V4M, V5M, V6M, V7M, V8M, V9M ,V10M, V11M, V12M, V13M, V14M, V15M, V16M,
+     V1SAD1_Window, V2SAD1_Window, V3SAD1_Window, V4SAD1_Window, V5SAD1_Window, 
+     V6SAD1_Window, V7SAD1_Window, V8SAD1_Window, V9SAD1_Window, V10SAD1_Window, V11SAD1_Window, 
+     V12SAD1_Window, V13SAD1_Window, V14SAD1_Window, V15SAD1_Window, V16SAD1_Window, 
+     V1SAD1_Frame, V2SAD1_Frame, V3SAD1_Frame, V4SAD1_Frame, V5SAD1_Frame, V6SAD1_Frame, 
+    V7SAD1_Frame, V8SAD1_Frame, V9SAD1_Frame, V10SAD1_Frame, V11SAD1_Frame, V12SAD1_Frame, V13SAD1_Frame, 
+    V14SAD1_Frame, V15SAD1_Frame, V16SAD1_Frame, FADM, FADSAD1);
     
-    SAD1_sub_mod SAD1_sub_mod(vector1, vector2, vector4);
+    SAD1_sub_mod SAD1_sub_mod(V1SAD1_Frame, V2SAD1_Frame, V3SAD1_Frame, V4SAD1_Frame, V5SAD1_Frame, V6SAD1_Frame, 
+    V7SAD1_Frame, V8SAD1_Frame, V9SAD1_Frame, V10SAD1_Frame, V11SAD1_Frame, V12SAD1_Frame, V13SAD1_Frame, 
+    V14SAD1_Frame, V15SAD1_Frame, V16SAD1_Frame,
+     V1SAD1_Window, V2SAD1_Window, V3SAD1_Window, V4SAD1_Window, V5SAD1_Window, 
+     V6SAD1_Window, V7SAD1_Window, V8SAD1_Window, V9SAD1_Window, V10SAD1_Window, V11SAD1_Window, 
+     V12SAD1_Window, V13SAD1_Window, V14SAD1_Window, V15SAD1_Window, V16SAD1_Window,
+     V1SAD1, V2SAD1, V3SAD1, V4SAD1, V5SAD1, V6SAD1, V7SAD1, V8SAD1, V9SAD1, V10SAD1, 
+    V11SAD1, V12SAD1, V13SAD1, V14SAD1, V15SAD1, V16SAD1);
     
     pipline_SAD1 pipline_SAD1(Clk, MemtoRegSAD1, RegWriteSAD1, MemReadDataSAD1, ALUResultSAD1, WriteRegSAD1,
     MemtoRegSAD2, RegWriteSAD2, MemReadDataSAD2, ALUResultSAD2, WriteRegSAD2,PCPlus4SAD1, 
     PCPlus4SAD2, jalSAD1, jalSAD2, DisplaySAD1, DisplaySAD2, BranchTypeSAD1, BranchTypeSAD2, Reset,
-    hazardTypeSAD1, hazardTypeSAD2, instructionSAD1, instructionSAD2, BranchSAD1, BranchSAD2);
+    hazardTypeSAD1, hazardTypeSAD2, instructionSAD1, instructionSAD2, BranchSAD1, BranchSAD2, SADSAD1, SADSAD2,
+    V1SAD1, V2SAD1, V3SAD1, V4SAD1, V5SAD1, V6SAD1, V7SAD1, V8SAD1, V9SAD1, V10SAD1, 
+    V11SAD1, V12SAD1, V13SAD1, V14SAD1, V15SAD1, V16SAD1, V1SAD2, V2SAD2, V3SAD2, V4SAD2, V5SAD2, V6SAD2, V7SAD2, 
+    V8SAD2, V9SAD2, V10SAD2, V11SAD2, V12SAD2, V13SAD2, V14SAD2, V15SAD2, V16SAD2, FADSAD1, FADSAD2);
     
-    SAD2_sub_mod SAD2_sub_mod(vector4SAD2, x, y, SADValue, instructionSAD2, xnew, ynew);
+    SAD2_sub_mod SAD2_sub_mod(V1SAD2, V2SAD2, V3SAD2, V4SAD2, V5SAD2, V6SAD2, V7SAD2, 
+    V8SAD2, V9SAD2, V10SAD2, V11SAD2, V12SAD2, V13SAD2, V14SAD2, V15SAD2, V16SAD2, XcurrW, YcurrW, 
+    currSADValueSAD2, instructionSAD2, xcurrSAD2, ycurrSAD2);
     
-    Pipline_SAD2 Pipline_SAD2(Clk, MemtoRegSAD2, RegWriteSAD2, MemReadDataSAD2, ALUResultSAD2, WriteRegSAD2,
+    pipline_SAD2 pipline_SAD2(Clk, MemtoRegSAD2, RegWriteSAD2, MemReadDataSAD2, ALUResultSAD2, WriteRegSAD2,
     MemtoRegW, RegWriteW, MemReadDataW, ALUResultW, WriteRegW,
     PCPlus4SAD2, PCPlus4W, jalSAD2, jalW, DisplaySAD2, DisplayW, BranchTypeSAD2, BranchTypeW, Reset,
-    hazardTypeW, hazardTypeSAD2, instructionSAD2, instructionW, BranchSAD2, BranchW);
+    hazardTypeW, hazardTypeSAD2, instructionSAD2, instructionW, BranchSAD2, BranchW, FADSAD2, FADW, xcurrSAD2, 
+    ycurrSAD2, minX, minY, currSADValueSAD2, min, XcurrW, YcurrW, minXW, minYW, currSADValueW, minW);
 
-    //Writeback Stage
-    //Mux32Bit2To1 MemToRegMux(WriteDataW1, ALUResultW, MemReadDataW, MemtoRegW);
-    //Mux32Bit2To1 JALMuxData(WriteDataW, WriteDataW1, (PCPlus4W), jalW);
+
+
     
-    Writeback_sub_mod(ALUResultW, MemReadDataW, MemtoRegW, PCPlus4W, jalW, WriteDataW, A, B, Xa, 
-    Ya, Xb, Yb, min, minX, minY);
+    Writeback_sub_mod Writeback_sub_mod(ALUResultW, MemReadDataW, MemtoRegW, PCPlus4W, jalW, WriteDataW, minW,
+     CurrSADValuew, minXW, minYW, XcurrW, YcurrW, min, minX, minY);
     
     
     
