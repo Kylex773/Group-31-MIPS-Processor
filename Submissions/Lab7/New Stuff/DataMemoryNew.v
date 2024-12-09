97799ba7aa0fd32cb1300034f7261bd9e1be1315
atsingine <atsingine@arizona.edu>
2024-12-08 20:39:00 -0700

    Update DataMemoryNew.v

--- Submissions/Lab7/New Stuff/DataMemoryNew.v
+++ Submissions/Lab7/New Stuff/DataMemoryNew.v
@@ -49,18 +49,18 @@ module DataMemoryNew(Address, WriteData, Clk, MemWrite, MemRead, ReadData, Reset
             vector[1] <= memory[Address[13:2] + 1];
             vector[2] <= memory[Address[13:2] + 2];
             vector[3] <= memory[Address[13:2] + 3];
-            vector[4] <= memory[Address[13:2] + 4];
-            vector[5] <= memory[Address[13:2] + 5];
-            vector[6] <= memory[Address[13:2] + 6];
-            vector[7] <= memory[Address[13:2] + 7];
-            vector[8] <= memory[Address[13:2] + 8];
-            vector[9] <= memory[Address[13:2] + 9];
-            vector[10] <= memory[Address[13:2] + 10];
-            vector[11] <= memory[Address[13:2] + 11];
-            vector[12] <= memory[Address[13:2] + 12];
-            vector[13] <= memory[Address[13:2] + 13];
-            vector[14] <= memory[Address[13:2] + 14];
-            vector[15] <= memory[Address[13:2] + 15];
+            vector[4] <= memory[Address[13:2] + 64];
+            vector[5] <= memory[Address[13:2] + 65];
+            vector[6] <= memory[Address[13:2] + 66];
+            vector[7] <= memory[Address[13:2] + 67];
+            vector[8] <= memory[Address[13:2] + 128];
+            vector[9] <= memory[Address[13:2] + 129];
+            vector[10] <= memory[Address[13:2] + 130];
+            vector[11] <= memory[Address[13:2] + 131];
+            vector[12] <= memory[Address[13:2] + 192];
+            vector[13] <= memory[Address[13:2] + 193];
+            vector[14] <= memory[Address[13:2] + 194];
+            vector[15] <= memory[Address[13:2] + 195];
         end
         else begin
             if(MemRead)
