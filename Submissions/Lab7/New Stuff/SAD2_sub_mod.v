a1d7ac5ef48c82c3a122647d3d4d50ae2820edd0
Kylex773 <kyle.carlsen77@gmail.com>
2024-12-08 23:04:42 -0700

    Update SAD2_sub_mod.v

--- Submissions/Lab7/New Stuff/SAD2_sub_mod.v
+++ Submissions/Lab7/New Stuff/SAD2_sub_mod.v
@@ -20,19 +20,35 @@
 //////////////////////////////////////////////////////////////////////////////////
 
 
-module SAD2_sub_mod(vector4SAD2, x, y, SADValue, instructionSAD2, xnew, ynew);
+module SAD2_sub_mod(V1, V2, V3, V4, V5, V5, V6, V7, V8, V9,
+V10, V11, V12, V13, V14, V15, V16,
+x, y, SADValue, instructionSAD2, xnew, ynew);
 
-input [31:0] vector4SAD2 [0:15];
+input [31:0] V1;
+input [31:0] V2;
+input [31:0] V3;
+input [31:0] V4;
+input [31:0] V5;
+input [31:0] V6;
+input [31:0] V7;
+input [31:0] V8;
+input [31:0] V9;
+input [31:0] V10;
+input [31:0] V11;
+input [31:0] V12;
+input [31:0] V13;
+input [31:0] V14;
+input [31:0] V15;
+input [31:0] V16;
+
+
 input [31:0] x, y;
 input [31:0] instructionSAD2;
 
 output reg [31:0] SADValue, xnew, ynew;
 
-Giant_Adder Giant_Adder(SADValue, vector4SAD2[0], vector4SAD2[1], vector4SAD2[2],
- vector4SAD2[3], vector4SAD2[4], vector4SAD2[5],
- vector4SAD2[6], vector4SAD2[7], vector4SAD2[8], vector4SAD2[9],
- vector4SAD2[10], vector4SAD2[11], vector4SAD2[12],
- vector4SAD2[13], vector4SAD2[14], vector4SAD2[15]);
+Giant_Adder Giant_Adder(SADValue, V1, V2, V3, V4, V5, V6, V7, V8, V9, V10, V11, V12, V13,
+V14, V15, V16);
 
 xyUpdate xyUpdate(x, y, instructionSAD2, xnew, ynew);
 
